ecd61e944418ddf804e57db48710f07ee423ffd4
Jie Liu <jieliunju@gmail.com>
2020-03-11 16:27:25 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The makeCacheMatrix return the list of 4 functions. 
+##It will put the matrix and the calculated inverse matrix in cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- matrix()
+  set <- function(y = matrix()){
+  x <<- y
+  m <<- matrix()
+  }
+  get <- function() x 
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+##This function will first check if the inverse matrix m in cache is NA. 
+##If it is NA, it will calculate and return the m. if it is not, it will take from cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(all(!is.na(m))) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
